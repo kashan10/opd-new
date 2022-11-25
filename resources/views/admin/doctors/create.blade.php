f4de887862253a27f71b5390872a6816a298096c
DESKTOP-88T01IL\kushan <kushanmaduranga10@gmail.com>
2022-11-25 07:36:07 +0530

    doc create blade

--- resources/views/admin/doctors/create.blade.php
+++ resources/views/admin/doctors/create.blade.php
@@ -9,7 +9,7 @@
          </div><br>
 
         <div class="d-sm-flex justify-content-between align-items-center mb-4">
-            <h3 class="text-dark mb-0">Nurse Details</h3>
+            <h3 class="text-dark mb-0">Doctor Details</h3>
         </div>
     </div>
 
@@ -19,7 +19,7 @@
     <form>
         <div class="card shadow mb-3">
                 <div class="card-header py-3">
-                    <p class="text-primary m-0 fw-bold" style="align:center">Nurse Registration</p>
+                    <p class="text-primary m-0 fw-bold" style="align:center">Doctor Registration</p>
                 </div>
 
                 <div class="card-body" style="margin-bottom: 5px; padding-bottom: 0px;">
@@ -32,91 +32,126 @@
                         <span class="invalid-feedback" role="alert">
                             <strong>{{ $message }}</strong>
                         </span>
-                        @enderror
+                    @enderror
                     </div>
-
 
                     <div class="col-sm-12 col-md-4 col-lg-4">
                         <div class="mb-3"><label class="form-label" for="NIC"><strong>NIC:&nbsp;</strong></label>
-                            <input class="form-control" type="text" id="NIC" placeholder="751584753v" name="NIC" maxlength="12" required>
+                            <input class="form-control" type="text" id="NIC" placeholder="751584753v" name="nic" maxlength="12" required>
                         </div>
                         @error('NIC')
-                        <span class="invalid-feedback" role="alert">
-                            <strong>{{ $message }}</strong>
-                        </span>
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
                         @enderror
                     </div>
 
                     <div class="col-sm-12 col-md-4 col-lg-4">
                         <div class="mb-3">
                             <label class="form-label" for="age"><strong>Age:</strong><br></label>
-                            <input class="form-control" type="number" placeholder="25 years" min="20" max="80" name="age">
+                            <input class="form-control" type="number" name="age" placeholder="25 years" min="20" max="80">
                         </div>
                         @error('age')
-                        <span class="invalid-feedback" role="alert">
-                            <strong>{{ $message }}</strong>
-                        </span>
-                        @enderror
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                         @enderror
                     </div>
 
                     <div class="mb-3"><label class="form-label" for="gender"><strong>Gender:</strong><br></label>
                         <div class="form-group mb-3">
                             <div class="form-check">
-                                <input class="form-check-input" type="radio" id="service_client_payment_validated-2" name="RadioOption" required>
+                                <input class="form-check-input" type="radio" id="service_client_payment_validated-2" name="gender" required>
                                 <label class="form-check-label" for="male">Male</label>
                             </div>
                             <div class="form-check">
-                                <input class="form-check-input" type="radio" id="service_client_payment_validated-1" name="RadioOption" required>
+                                <input class="form-check-input" type="radio" id="service_client_payment_validated-1" name="gender" required>
                                 <label class="form-check-label" for="female">Female</label>
                             </div>
+                            @error('gender')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                         @enderror
                         </div>
                     </div>
 
                     <div class="col-md-4">
                         <label class="form-label" for="client_description"><strong>Address:</strong><br></label>
-                        <textarea class="form-control" id="service_description" rows="5" name="service_description" placeholder="No.31, Market Road, Main-Street, Matara." required=""></textarea>
-                    </div><br>
+                        <textarea class="form-control" id="service_description" rows="5" name="address" placeholder="No.31, Market Road, Main-Street, Matara." required=""></textarea>
+                        @error('address')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                             @enderror
+                    </div>
+                    <br>
 
                     <div class="col-sm-12 col-md-4 col-lg-4">
                         <div class="mb-3"><label class="form-label" for="email"><strong>Email:&nbsp;</strong></label>
-                            <input class="form-control" type="email" id="email" placeholder="pathirana@gmail.com" name="email" required>
+                            <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+                            @error('email')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                             @enderror
                         </div>
                     </div>
 
                     <div class="col-sm-12 col-md-4 col-lg-4">
-                        <div class="mb-3"><label class="form-label" for="telephone_no"><strong>Telephone Number:&nbsp;</strong></label>
-                            <input class="form-control" type="telno" id="telno" placeholder="07X XXX XXXX" name="telno" maxlength="12" required>
+                        <div class="mb-3"><label class="form-label" for="telephone_no"><strong>Telephone Number:</strong></label>
+                            <input class="form-control" type="telno" id="telno" placeholder="07X XXX XXXX" name="phone" maxlength="12" required>
                         </div>
+                        @error('phone')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                             @enderror
                     </div>
 
                     <div class="col-sm-12 col-md-4 col-lg-4">
                         <div class="mb-3"><label class="form-label" for="Position"><strong>Position</strong><br></label>
                             <input class="form-control" type="text" id="position" placeholder="Grade I" name="position" required>
                         </div>
+                        @error('position')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                     @enderror
                     </div>
 
                     <div class="col-sm-12 col-md-4 col-lg-4">
-                        <div class="mb-3"><label class="form-label" for="qualification"><strong>Qualification:</strong><br></label>
-                            <input class="form-control" type="text" id="qualification" placeholder="GCE A/L Qualified" name="qualification" required>
+                        <div class="mb-3"><label class="form-label" for="qualification"><strong>"Specialization:</strong><br></label>
+                            <input class="form-control" type="text" id="qualification" placeholder="GCE A/L Qualified" name="specialization" required>
                         </div>
+                        @error('specialization')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                     @enderror
                     </div>
                     
-                    <div class="mb-3"><label class="form-label" for="service_client_payment_validated"><strong>Status:</strong><br></label>
+                    {{-- <div class="mb-3"><label class="form-label" for="service_client_payment_validated"><strong>Status:</strong><br></label>
                         <div class="form-group mb-3">
                             <div class="form-check">
-                                <input class="form-check-input" type="radio" id="formCheck-1">
+                                <input class="form-check-input" type="radio" name="status" id="formCheck-1">
                                 <label class="form-check-label" for="formCheck-1">Active</label>
                             </div>
                             <div class="form-check">
-                              <input class="form-check-input" type="radio" id="formCheck-2">
+                              <input class="form-check-input" type="radio" name="status" id="formCheck-2">
                               <label class="form-check-label" for="formCheck-2">Inactive</label>
                             </div>
                         </div>
-                    </div>
+                    </div> --}}
 
                     <div class="col-lg-4">
                         <div class="mb-3"><label class="form-label" for="service_client_start_date"><strong>Uplaod a Photo:</strong><br></label>
-                        <input class="form-control" type="file">
+                        <input class="form-control" type="file" name="file">
+                        @error('file')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                     @enderror
                     </div>
 
                 </div>
@@ -125,10 +160,21 @@
                     <div class="container">
                         <div class="row">
                             <div class="col-lg-4">
-                                <div class="mb-3"><label class="form-label" for="service_client_start_date"><strong>Set Password:</strong><br></label><input class="form-control" type="password"></div>
+                                <div class="mb-3">
+                                    <label class="form-label" for="service_client_start_date"><strong>Set Password:</strong><br></label>
+                                    <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+                                    @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                                </div>
                             </div>
                             <div class="col-lg-4">
-                                <div class="mb-3"><label class="form-label" for="service_client_start_date"><strong>Confirm Password:</strong><br></label><input class="form-control" type="password"></div>
+                                <div class="mb-3">
+                                    <label class="form-label" for="service_client_start_date"><strong>Confirm Password:</strong><br></label>
+                                    <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -137,8 +183,8 @@
         </div>
         
         <div class="text-center mb-3">
-            <button class="btn btn-primary btn-lg" type="submit">Save&nbsp;</button>
-            <button class="btn btn-danger btn-lg" type="reset">Reset&nbsp;</button>
+            <button class="btn btn-primary btn-lg" type="submit">Save</button>
+            <button class="btn btn-danger btn-lg" type="reset">Reset</button>
         </div>
         
     </form>
